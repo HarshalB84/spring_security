964392b794032caf28df315046a4c43bc4892571
HarshalB84 <harshalbhoyar84@gmail.com>
2026-01-16 23:07:01 +0530

    Spring Security Details added with Authentication Manager

---
+++ src/main/java/com/practice/SecurityApp/SecurityApplication/entities/User.java
@@ -0,0 +1,36 @@
+package com.practice.SecurityApp.SecurityApplication.entities;
+
+import jakarta.persistence.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
